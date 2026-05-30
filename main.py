5f7c080d0791e6f36d67e52e574ed7175bf78515
Ashrita-smart <ashritapaladugu@gmail.com>
2026-05-30 15:24:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ def digitsum(count,n):
         return 0
     if n==1:
         return 1
-    if count==0:
-        return 0
-    if count==1:
-        return 1
+
     return digitsum(count+n)
 print(digitsum(0,int(input("Enetr the number"))))
